0c36979873b3592c9cf7e5b5030042c9f2573a92
istutiupadhyay <66789421+istutiupadhyay@users.noreply.github.com>
2023-02-10 08:53:35 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -6,5 +6,5 @@ const userController = require('../controllers/user');
 
 const router = express.Router();
 router.post('/user/signup', userController.newuser);
-//router.post('/user/login', userController.existinguser);
+router.post('/user/login', userController.existinguser);
 module.exports=router;
