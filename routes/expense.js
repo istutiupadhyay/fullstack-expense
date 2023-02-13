51b79da80d9f220d2817ca5f80ba6a12b7406862
istutiupadhyay <66789421+istutiupadhyay@users.noreply.github.com>
2023-02-13 11:32:45 +0530

    Update expense.js

--- routes/expense.js
+++ routes/expense.js
@@ -2,8 +2,8 @@ const path=require('path');
 const express=require('express');
 const expenseController=require('../controllers/expense')
 const router=express.Router();
-router.post('/add-expense', expenseController.postexpense);
-router.get('/get-expense', expenseController.getexpense);
+router.post('/add-expense', userauthentication.authenticate , expenseController.postexpense);
+router.get('/get-expense', userauthentication.authenticate , expenseController.getexpense);
 router.delete('/delete-expense/:id', expenseController.deleteexpense);
 
 module.exports=router;
