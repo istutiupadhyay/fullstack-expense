518711566c3e4c4d52f8f97827623d21dd25aa0d
istutiupadhyay <66789421+istutiupadhyay@users.noreply.github.com>
2023-02-15 23:32:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,14 +13,18 @@ app.use(express.json());
 const userRoutes=require('./routes/user');
 const expenseRoutes=require('./routes/expense');
 const premiumRoutes=require('./routes/premium');
+const resetPasswordRoutes = require('./routes/resetpassword');
 
 const Expuser=require('./models/user');
 const Expense=require('./models/expense');
 const Order = require('./models/order');
+const Forgotpassword = require('./models/forgotpassword');
 
 app.use(userRoutes)
 app.use(expenseRoutes);
 app.use('/purchase',premiumRoutes);
+app.use('/password', resetPasswordRoutes);
+
 
 app.set('views', 'views');
 //app.use(bodyParser.json());
@@ -31,6 +35,9 @@ Expense.belongsTo(Expuser);
 
 Expuser.hasMany(Order);
 Order.belongsTo(Expuser);
+
+Expuser.hasMany(Forgotpassword);
+Forgotpassword.belongsTo(Expuser);
 
 sequelize
 .sync()
