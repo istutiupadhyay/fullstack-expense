ed8e4b6547fa6741da84915d2e1d353dc4936bc7
istutiupadhyay <66789421+istutiupadhyay@users.noreply.github.com>
2023-03-12 23:22:45 +0530

    Delete routes directory

--- routes/expense.js
+++
@@ -1,9 +0,0 @@
-const path=require('path');
-const express=require('express');
-const expenseController=require('../controllers/expense')
-const router=express.Router();
-router.post('/add-expense', userauthentication.authenticate , expenseController.postexpense);
-router.get('/get-expense', userauthentication.authenticate , expenseController.getexpense);
-router.delete('/delete-expense/:id', expenseController.deleteexpense);
-
-module.exports=router;
--- routes/premiumfeature.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const premiumfeatureController = require('../controllers/premiumfeature');
-const authenticatemiddleware = require('../middleware/auth');
-const router = express.router();
-router.get('/showleaderboard', authenticatemiddleware.authenticate.premiumfeatureController.getleaderboard);
-module.exports = router;
